402c2ac5ed815d18b63cfad05786feb5dedbb66d
whoamif <adjissifatimaamina@gmail.com>
2024-02-01 00:41:08 +0100

    up

--- frontend/homepage/src/login/src/Login.jsx
+++ frontend/homepage/src/login/src/Login.jsx
@@ -15,7 +15,7 @@ const handleLogin = (email, password, navigate) => {
   axios
     .post("http://127.0.0.1:8000/auth/jwt/create/", data)
     .then((response) => {
-      console.log(response);
+      console.log(response.json);
       localStorage.setItem("token", response.data.access);
       navigate("/");
     })
