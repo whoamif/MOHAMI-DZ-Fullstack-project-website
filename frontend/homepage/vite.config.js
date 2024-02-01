7499515d7f165afc122705156abdc99e5ea4a8b5
Manel Derrouaz <derrouazmanel@gmail.com>
2024-02-01 17:38:31 +0100

    up

--- frontend/homepage/vite.config.js
+++ frontend/homepage/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import React from 'react';
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [React(), VitePWA()],})
+  plugins: [react(), VitePWA()],
+});
